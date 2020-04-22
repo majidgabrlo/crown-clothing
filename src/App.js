b8cb42ef9353883fe50094f2c568d338a8490d8d
majidgabrlo <61796004+majidgabrlo@users.noreply.github.com>
2020-04-23 00:04:43 +0430

    update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage-component';
 
+
+const HatsPage=()=>(
+  <h1>Hats!</h1>
+)
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* سوییج اولین مسیر پیدا شده را لود میکند و دیگر صورت وجود آدرس های پیدا شده مابقی را لود نمیکند */}
+      <Switch>
+        {/* دستور زیر برای روتینگ است  */}
+        {/* "exact" shows that it have to be "/" to load HomePage */}
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
